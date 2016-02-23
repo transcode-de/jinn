ebedd1e12e1ffc3a43d5d7b45890e5d55a2c1359
René Muhl <rene@transcode.de>
2016-02-23 15:46:25 +0100

    Added work around to call and list our tasks

--- jinn/main.py
+++ jinn/main.py
@@ -1,6 +1,16 @@
-from invoke import Program
-from invoke import Collection
+from invoke import Collection, Program
+from invoke.parser import ParserContext
 from . import tasks
 
 
-program = Program(namespace=Collection.from_module(tasks), version='0.1.0')
+class MyProgram(Program):
+    """Until the problem with https://github.com/pyinvoke/invoke/issues/283
+    is fixed we need this work around class and property.
+    """
+    @property
+    def initial_context(self):
+        args = super().core_args()
+        args += super().task_args()
+        return ParserContext(args=args)
+
+program = MyProgram(namespace=Collection.from_module(tasks), version='0.1.0')
